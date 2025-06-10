0c19b3c9cc7252275daa5d15ed971448941f0652
cybernetic-m <romano.massimo.uni@gmail.com>
2025-06-10 12:42:16 +0200

    write the basic while

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import gymnasium as gym
 
 env = gym.make("Reacher-v5", render_mode="human")
+
+# Reset the environment to start a new episode
+# Return the initial observation and info dictionary (if available)
+observation, info = env.reset()
+
+done = False
+
+while not done:
+   
+    # Sample a random action from the action space
+    action = env.action_space.sample()
+
+    # Step the environment with the sampled action
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    # Render the environment
+    env.render()
+
+    # Check if the episode is done
+    done = terminated or truncated
+
+env.close()
