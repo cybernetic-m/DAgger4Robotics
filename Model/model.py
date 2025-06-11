0e4325a26a3530f9c15be3a48afa30ff74986a65
Luca <lucadelsignore1998@gmail.com>
2025-06-11 12:44:33 +0200

    minari_policy

--- Model/model.py
+++ Model/model.py
@@ -13,3 +13,16 @@ class Expert_policy(nn.Module):
         )
     def forward(self, x):
         return self.net(x)
+    
+class Minari_policy(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, output_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
