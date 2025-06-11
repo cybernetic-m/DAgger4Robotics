58791abe878cbf8601e491dedba29befa1122d98
cybernetic-m <romano.massimo.uni@gmail.com>
2025-06-11 12:59:55 +0200

    push of modification

---
+++ utils/collate_fn.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn.utils.rnn.pad_sequence as pad_sequence
+
+def collate_fn(batch):
+
+    return {
+        "id": torch.Tensor([x.id for x in batch]),
+        "observations": pad_sequence([torch.as_tensor(x.observations) for x in batch], batch_first=True),
+        "actions": pad_sequence([torch.as_tensor(x.actions) for x in batch], batch_first=True),
+        "rewards": pad_sequence([torch.as_tensor(x.rewards) for x in batch], batch_first=True),
+        "terminations": pad_sequence([torch.as_tensor(x.terminations) for x in batch], batch_first=True),
+        "truncations": pad_sequence([torch.as_tensor(x.truncations) for x in batch], batch_first=True),
+    }
