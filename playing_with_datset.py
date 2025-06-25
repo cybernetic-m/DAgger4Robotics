f6802cf94fd9d42b1ede58186d67389c361b890a
Luca <lucadelsignore1998@gmail.com>
2025-06-25 10:46:51 +0200

    loader for kitchen dataset

--- playing_with_datset.py
+++ playing_with_datset.py
@@ -3,11 +3,20 @@ import gymnasium as gym
 from PIL import Image
 import os
 import numpy as np
+from utils.state_action_to_loader import state_action_to_loader
+from utils.preprocess_dataset import preprocess_dataset
 
 dictio={'tasks_to_complete': ['microwave']}
 dataset = minari.load_dataset('D4RL/kitchen/complete-v2', download=True)
 
+print(dataset.total_episodes)
+microwave_dataset = preprocess_dataset(dataset)
+loader=state_action_to_loader(microwave_dataset,256)
 
+for obs_batch, act_batch in loader:
+    print("Obs batch shape:", obs_batch.shape)
+    print("Action batch shape:", act_batch.shape)
+    break
 
 
 #env = dataset.recover_environment()
@@ -17,8 +26,11 @@ dataset = minari.load_dataset('D4RL/kitchen/complete-v2', download=True)
 p1=dataset[14].observations['achieved_goal']['microwave'].flatten()
 p2=dataset[14].observations["observation"][:,31].flatten()
 
-print(dataset[4].observations['desired_goal']['microwave'])
-
+#print(dataset[4].observations['desired_goal']['microwave'])
+x=dataset[4]
+x=[2,3,4]
+print(hasattr(x, '__class__') and x.__class__.__module__ != 'builtins')
+print()
 print(dataset[4])
 """
 i=0
--- utils/state_action_to_loader.py
+++ utils/state_action_to_loader.py
@@ -6,18 +6,29 @@ import numpy as np
 def state_action_to_loader(data, batch_size):
     x_list = []  # observations (input for the model)
     y_list = []  # actions (target)
-    for ep in data:
-        obs = ep.observations[:-1]  # (50, 10) (removing the last one)
-        acts = ep.actions           # (50, 2)
+    if hasattr(data, '__class__') and data.__class__.__module__ != 'builtins':
+        for ep in data:
+            obs = ep.observations[:-1]  # (50, 10) (removing the last one)
+            acts = ep.actions           # (50, 2)
+            
+            # consistency check
+            assert obs.shape[0] == acts.shape[0], f"Shape mismatch: {obs.shape[0]} vs {acts.shape[0]}"
+            
+            x_list.append(obs)
+            y_list.append(acts)
         
-        # consistency check
-        assert obs.shape[0] == acts.shape[0], f"Shape mismatch: {obs.shape[0]} vs {acts.shape[0]}"
-        
-        x_list.append(obs)
-        y_list.append(acts)
-    
-    s = np.concatenate(x_list, axis=0)  # shape (len(data)*50, 10)
-    a = np.concatenate(y_list, axis=0)  # shape (len(data)*50, 2)
+        s = np.concatenate(x_list, axis=0)  # shape (len(data)*50, 10)
+        a = np.concatenate(y_list, axis=0)  # shape (len(data)*50, 2)
+    else:
+        for ep_id in data['observations']:
+            obs = data['observations'][ep_id]
+            acts = data['actions'][ep_id]
+
+            x_list.extend(obs)
+            y_list.extend(acts)
+            
+        s = np.array(x_list)
+        a = np.array(y_list)
 
     dataset = TensorDataset(torch.tensor(s, dtype=torch.float32), torch.tensor(a, dtype=torch.float32))
     return DataLoader(dataset, batch_size=batch_size, shuffle=True)
